ef34794c4a24ee0dfa830fc57547c2d50d141a35
FunkyToc <vaysse.jeanbaptiste@gmail.com>
2022-10-12 11:28:04 +0200

    freelook input

---
+++ Assets/Scripts/ViewInput.cs
@@ -0,0 +1,41 @@
+using Cinemachine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ViewInput : MonoBehaviour
+{
+    [SerializeField] InputActionReference _zoomInput;
+    [SerializeField] CinemachineFreeLook _freelookCam;
+
+    bool isZooming = false;
+
+    void Start()
+    {
+        _zoomInput.action.started += ZoomStarted;
+        _zoomInput.action.canceled += ZoomCanceled;
+    }
+
+    void OnDestroy()
+    {
+        _zoomInput.action.started -= ZoomStarted;
+        _zoomInput.action.canceled -= ZoomCanceled;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void ZoomStarted(InputAction.CallbackContext obj)
+    {
+        _freelookCam.gameObject.SetActive(true);
+    }
+    
+    private void ZoomCanceled(InputAction.CallbackContext obj)
+    {
+        _freelookCam.gameObject.SetActive(false);
+    }
+}
